0186180767d46d986b166efa455d04ed7de79784
Dmitry Bubyakin <dimabubyakin@mail.ru>
2019-02-01 11:42:25 +0200

    Fix

--- resources/js/components/Medialibrary/File/File.vue
+++ resources/js/components/Medialibrary/File/File.vue
@@ -55,7 +55,7 @@ export default {
         },
 
         visibleLabels () {
-            return this.file.labels.filter(label => label.visible)
+            return (this.file.labels || []).filter(label => label.visible)
         }
     }
 }
