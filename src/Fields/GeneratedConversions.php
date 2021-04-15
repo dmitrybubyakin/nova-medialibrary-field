8041fe6482082f850ecc1c389df7e32062ad9d37
Monaye Win <>
2021-04-15 15:33:47 -0700

    Added copyAs and previewUsing for GeneratedConversions

--- src/Fields/GeneratedConversions.php
+++ src/Fields/GeneratedConversions.php
@@ -4,22 +4,33 @@ namespace DmitryBubyakin\NovaMedialibraryField\Fields;
 
 use Laravel\Nova\Fields\Field;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use function DmitryBubyakin\NovaMedialibraryField\validate_args;
+use function DmitryBubyakin\NovaMedialibraryField\callable_or_default;
 
 class GeneratedConversions extends Field
 {
     public $component = 'nova-generated-conversions-field';
 
+    private $previewCallback;
+    private $copyAsCallback;
+
     public $showOnIndex = false;
 
     public function __construct($name)
     {
+        $this->previewUsing();
+        $this->copyAs();
+
         parent::__construct($name, function (Media $media) {
             return $media
                 ->getGeneratedConversions()
                 ->filter()
                 ->keys()
                 ->mapWithKeys(function (string $conversionName) use ($media) {
-                    return [$conversionName => $media->getFullUrl($conversionName)];
+                    return [$conversionName => [
+                        'previewUrl' => call_user_func($this->previewCallback, $media, $conversionName),
+                        'copyAsUrl' => call_user_func($this->copyAsCallback, $media, $conversionName)
+                    ]];
                 });
         });
     }
@@ -27,5 +38,39 @@ class GeneratedConversions extends Field
     public function withTooltips(bool $withTooltips = true): self
     {
         return $this->withMeta(['withTooltips' => $withTooltips]);
+    }
+
+    /**
+     * @param string|callable|null $previewUsing
+     */
+    public function previewUsing($previewUsing = null): self
+    {
+        validate_args();
+
+        $this->previewCallback = callable_or_default(
+            $previewUsing,
+            function (Media $media, string $conversionName) use ($previewUsing): ?string {
+                return $media->getFullUrl($previewUsing ?: $conversionName);
+            }
+        );
+
+        return $this;
+    }
+
+    /**
+     * @param string|callable|null copyAs$
+     */
+    public function copyAs($copyAs = null): self
+    {
+        validate_args();
+
+        $this->copyAsCallback = callable_or_default(
+            $copyAs,
+            function (Media $media, string $conversionName) use ($copyAs): ?string {
+                return $media->getFullUrl($copyAs ?: $conversionName);
+            }
+        );
+
+        return $this;
     }
 }
