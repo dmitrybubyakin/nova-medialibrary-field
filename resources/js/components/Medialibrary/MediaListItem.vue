514b9aea7c5e654648d77eaca1b98b348ab736fa
Jakub Theimer <jakub@dy.st>
2022-04-25 05:04:42 +0200

    Fix styles

--- resources/js/components/Medialibrary/MediaListItem.vue
+++ resources/js/components/Medialibrary/MediaListItem.vue
@@ -1,22 +1,22 @@
 <template>
-  <div v-tooltip="tooltip" class="dragging:border-none relative border border-40 rounded overflow-hidden shadow">
+  <div v-tooltip="tooltip" class="relative overflow-hidden rounded shadow dragging:border-none">
     <MediaListItemPreview :media="media">
-      <div class="dragging:hidden group-hover:block hidden absolute pin bg-overlay rounded-b">
-        <div class="h-full flex justify-center items-center">
-          <a :href="media.downloadUrl" target="_blank" class="media-item-download text-white">
-            <icon type="download" view-box="0 0 24 24" width="32" height="32" />
+      <div class="dragging:hidden group-hover:block hidden absolute inset-0 bg-overlay rounded-b">
+        <div class="flex items-center justify-center h-full">
+          <a :href="media.downloadUrl" target="_blank" class="text-white media-item-download">
+            <Icon type="download" view-box="0 0 24 24" width="32" height="32" />
           </a>
         </div>
       </div>
     </MediaListItemPreview>
 
-    <div v-if="media.title" class="dragging:hidden w-32 mt-1 px-2">
-      <p class="text-80 text-sm truncate">
+    <div v-if="media.title" class="w-32 px-2 mt-1 dragging:hidden">
+      <p class="text-sm truncate text-80">
         {{ media.title }}
       </p>
     </div>
 
-    <div v-if="media.loading" class="absolute pin bg-90-half flex items-center justify-center">
+    <div v-if="media.loading" class="absolute flex items-center justify-center inset-0 bg-overlay">
       <loader class="text-white" />
     </div>
 
@@ -27,6 +27,7 @@
 
 <script>
 import { tooltip } from './Utils'
+
 import MediaListItemActions from './MediaListItemActions'
 import MediaListItemPreview from './MediaListItemPreview'
 import MediaListItemModals from './MediaListItemModals'
