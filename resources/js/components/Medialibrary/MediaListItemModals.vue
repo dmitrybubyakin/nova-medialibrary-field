90607d2eebe3130fcdf1e7f642e6e0d8909d67e9
Jakub Theimer <jakub@dy.st>
2022-04-25 05:00:46 +0200

    Modals now use Vue teleport, utilise new default Nova DeleteResourceModal

--- resources/js/components/Medialibrary/MediaListItemModals.vue
+++ resources/js/components/Medialibrary/MediaListItemModals.vue
@@ -1,42 +1,44 @@
 <template>
-  <portal v-if="media.deleteModalOpen || media.detailModalOpen || media.updateModalOpen || media.cropperModalOpen" to="modals">
-    <delete-resource-modal
-      v-if="media.deleteModalOpen"
-      mode="delete"
-      @confirm="media.confirmDelete()"
-      @close="media.closeDeleteModal()"
-    />
+  <DeleteResourceModal
+    v-if="media.deleteModalOpen"
+    :show="media.deleteModalOpen"
+    mode="delete"
+    @confirm="media.confirmDelete()"
+    @close="media.closeDeleteModal()"
+  />
 
-    <EditModal
-      v-else-if="media.updateModalOpen"
-      :resource-name="media.resourceName"
-      :resource-id="media.resourceId"
-      :fields="media.fields"
-      :errors="media.errors"
-      :updating="media.updating"
-      @close="media.closeUpdateModal()"
-      @submit="media.update($event)"
-    />
+  <EditModal
+    v-else-if="media.updateModalOpen"
+    :show="media.updateModalOpen"
+    :resource-name="media.resourceName"
+    :resource-id="media.resourceId"
+    :fields="media.fields"
+    :errors="media.errors"
+    :updating="media.updating"
+    @close="media.closeUpdateModal()"
+    @submit="media.update($event)"
+  />
 
-    <CropperModal
-      v-else-if="media.cropperModalOpen"
-      :media="media"
-      :updating="media.updating"
-      @close="media.closeCropperModal()"
-      @crop="media.crop($event)"
-    />
+  <CropperModal
+    v-else-if="media.cropperModalOpen"
+    :show="media.cropperModalOpen"
+    :media="media"
+    :updating="media.updating"
+    @close="media.closeCropperModal()"
+    @crop="media.crop($event)"
+  />
 
-    <DetailModal
-      v-else-if="media.detailModalOpen"
-      :resource-name="media.resourceName"
-      :resource-id="media.resourceId"
-      :resource="media.resource"
-      :readonly="context.field.readonly"
-      @close="media.closeDetailModal()"
-      @delete="media.openDeleteModal()"
-      @edit="media.edit()"
-    />
-  </portal>
+  <DetailModal
+    v-else-if="media.detailModalOpen"
+    :show="media.detailModalOpen"
+    :resource-name="media.resourceName"
+    :resource-id="media.resourceId"
+    :resource="media.resource"
+    :readonly="context.field.readonly"
+    @close="media.closeDetailModal()"
+    @delete="media.openDeleteModal()"
+    @edit="media.edit()"
+  />
 </template>
 
 <script>
