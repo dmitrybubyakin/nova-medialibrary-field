f6f388d605b452a8270b1875841a3e6f1fc389ab
Jakub Theimer <jakub@dy.st>
2022-05-02 16:12:31 +0200

    style fix

--- resources/js/components/MedialibraryDetailField.vue
+++ resources/js/components/MedialibraryDetailField.vue
@@ -1,27 +1,30 @@
 <template>
-    <PanelItem :field="field">
-        <template #value>
-            <MedialibraryField :add-files="canAddFiles" :field="field" :resource-name="resourceName"
-                :resource-id="resourceId" />
-        </template>
-    </PanelItem>
+  <PanelItem :field="field">
+    <template #value>
+      <MedialibraryField
+        :add-files="canAddFiles"
+        :field="field"
+        :resource-name="resourceName"
+        :resource-id="resourceId"
+      />
+    </template>
+  </PanelItem>
 </template>
 
 <script>
 import MedialibraryField from './MedialibraryField'
 
 export default {
-    components: {
-        MedialibraryField,
-    },
+  components: {
+    MedialibraryField,
+  },
 
-    // eslint-disable-next-line
-    props: ['resource', 'resourceName', 'resourceId', 'field'],
+  props: ['resource', 'resourceName', 'resourceId', 'field'],
 
-    computed: {
-        canAddFiles() {
-            return !!this.field.attachOnDetails
-        },
+  computed: {
+    canAddFiles() {
+      return !!this.field.attachOnDetails
     },
+  },
 }
 </script>
