5491163b92f2a42a01c6ca24c659c178e9952b4b
marlo <corridor.marlo@gmail.com>
2022-09-30 14:16:09 +0800

    Add `Medialibrary` Field on constructor parameter
    Patterned on `DmitryBubyakin\NovaMedialibraryField\Fields\Support\MediaPresenter`

--- src/Fields/Support/AttachableMediaPresenter.php
+++ src/Fields/Support/AttachableMediaPresenter.php
@@ -2,16 +2,21 @@
 
 namespace DmitryBubyakin\NovaMedialibraryField\Fields\Support;
 
+use function DmitryBubyakin\NovaMedialibraryField\call_or_default;
+use DmitryBubyakin\NovaMedialibraryField\Fields\Medialibrary;
 use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Support\Str;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class AttachableMediaPresenter implements Arrayable
 {
     private $media;
+    private $field;
 
-    public function __construct(Media $media)
+    public function __construct(Media $media, Medialibrary $field)
     {
         $this->media = $media;
+        $this->field = $field;
     }
 
     public function toArray(): array
@@ -23,7 +28,22 @@ class AttachableMediaPresenter implements Arrayable
             'fileName' => $this->media->file_name,
             'collectionName' => $this->media->collection_name,
             'extension' => $this->media->extension,
-            'previewUrl' => $this->media->getFullUrl(),
+            'previewUrl' => $this->previewUrl(),
         ]);
+    }
+
+    public function previewUrl(): ?string
+    {
+        return transform(call_or_default($this->field->previewCallback, [$this->media], function () {
+            return Str::is('image/*', $this->media->mime_type)
+                ? $this->media->getFullUrl()
+                : null;
+        }), function (string $url): string {
+            if ($this->field->appendTimestampToPreview) {
+                return $url . '?timestamp=' . $this->media->updated_at->getTimestamp();
+            }
+
+            return $url;
+        });
     }
 }
